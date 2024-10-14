3d7214c0aff4c8b49046053db5bd8594ca91f313
supriyo <supriyo.bhakat@comprinno.net>
2024-10-14 16:11:43 +0530

    Policy-Length-14-Code-Optimised

--- library/aws/checks/iam/iam_password_policy_minimum_length_14.py
+++ library/aws/checks/iam/iam_password_policy_minimum_length_14.py
@@ -4,8 +4,6 @@ DATE: 2024-10-10
 """
 
 import boto3
-from botocore.exceptions import ClientError
-
 from tevico.engine.entities.report.check_model import CheckReport
 from tevico.engine.entities.check.check import Check
 
@@ -20,25 +18,18 @@ class iam_password_policy_minimum_length_14(Check):
         try:
             password_policy = client.get_account_password_policy()
             policy_exists = True
-        except ClientError as e:
-            # Handle specific errors related to the IAM policy retrieval
-            if e.response['Error']['Code'] == 'NoSuchEntity':
-                print("No IAM password policy found.")
-                policy_exists = False
-            else:
-                print(f"Unexpected error: {e}")  # Log unexpected errors
-                policy_exists = False  # Default to no policy if an unexpected error occurs
+        except Exception as e:
+            print("No IAM password policy found or an error occurred.")  # Handle all errors
+            policy_exists = False
         
         if policy_exists:
             # Check the length of the password policy
             password_policy_length = password_policy['PasswordPolicy'].get('MinimumPasswordLength', 0)
-            if password_policy_length >= 14:
-                report.passed = True
-            else:
-                report.passed = False
+            report.passed = password_policy_length >= 14
         else:
             report.passed = False
 
         # Report the status for the password policy check
         report.resource_ids_status['IAM Password Policy'] = report.passed
         return report
+
