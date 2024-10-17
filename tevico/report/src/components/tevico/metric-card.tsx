e4d65b82b856ac494c5e6128a84d5daf30331a26
Suhaib W <suhaib.wani@comprinno.net>
2024-10-17 15:27:57 +0530

    Fixed responsive behavior

--- tevico/report/src/components/tevico/metric-card.tsx
+++ tevico/report/src/components/tevico/metric-card.tsx
@@ -20,20 +20,22 @@ interface MetricCardProps {
 export function MetricCard(props: MetricCardProps) {
   return (
     <Card x-chunk="dashboard-01-chunk-0">
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">
+      <CardHeader className="flex flex-row justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium basis-2/3">
           {props.title}
         </CardTitle>
-        {(() => {
-          switch (props.icon) {
-            case 'users':
-              return <Users className="h-4 w-4 text-muted-foreground" />;
-            case 'dollar':
-              return <DollarSign className="h-4 w-4 text-muted-foreground" />;
-            default:
-              null;
-          }
-        })()}
+        <div className='basis-auto'>
+          {(() => {
+            switch (props.icon) {
+              case 'users':
+                return <Users className="h-4 w-4 text-muted-foreground" />;
+              case 'dollar':
+                return <DollarSign className="h-4 w-4 text-muted-foreground" />;
+              default:
+                null;
+            }
+          })()}
+        </div>
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">
