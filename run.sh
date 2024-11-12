3bec38a6bc5b23aa2858cfa857897f87e712d4ef
Abhay Pai <abhay.pai@comprinno.net>
2024-11-12 12:47:34 +0530

    refactor: added print statements to the run command

--- run.sh
+++ run.sh
@@ -1,9 +1,27 @@
+echo 'Setup in progress...\n'
+
+echo 'Cleaning up previous builds...\n'
 cd /tmp
 rm -rf tevico-community
+
+echo 'Cloning the repository...\n'
 git clone -b dev https://github.com/comprinnotech/tevico-community.git
 cd tevico-community
+
+echo 'Setting up virtual environment...\n'
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install poetry
-poetry install
+
+echo 'Installing dependencies...'
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+pip3 install poetry > /dev/null
+echo -ne '#############             (66%)\r'
+sleep 1
+poetry install > /dev/null
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+echo 'Running the application...\n'
 /tmp/tevico-community/main run
