4e99ed18fdc3b41cac652ae37be64a09e3399fa6
deepak-puri-comprinno <deepak.puri@comprinno.net>
2024-11-26 20:25:20 +0530

    refactor: Added exception handling in the check rds_instance_backup_enabled.py

--- library/aws/checks/rds/rds_instance_backup_enabled.py
+++ library/aws/checks/rds/rds_instance_backup_enabled.py
@@ -5,31 +5,30 @@ from tevico.engine.entities.report.check_model import CheckReport
 from tevico.engine.entities.check.check import Check
 
 
-class rds_instance_deletion_protection(Check):
+class rds_instance_backup_enabled(Check):
 
     def execute(self, connection: boto3.Session) -> CheckReport:
         report = CheckReport(name=__name__)
-        
+        report.passed = True
         try:
             client = connection.client('rds')
             instances = client.describe_db_instances()['DBInstances']
-            report.passed = True
             
             for instance in instances:
                 instance_name = instance['DBInstanceIdentifier']
+                backup_retention_period = instance['BackupRetentionPeriod']
                 
-                if instance['DeletionProtection']:
-                    report.resource_ids_status[instance_name] = True
-                else:
+                if backup_retention_period == 0:
                     report.passed = False
                     report.resource_ids_status[instance_name] = False
-                                         
+                else:
+                    report.resource_ids_status[instance_name] = True
+            
         except Exception as e:
             report.passed = False
             return report
 
         return report
-
 
 
     
