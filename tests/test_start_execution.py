66f170e59a252e61d7a2e286ec35ae64a5fa9374
deepak-puri-comprinno <deepak.puri@comprinno.net>
2025-01-02 15:54:28 +0530

    used start_execution function over the existing class

--- tests/test_start_execution.py
+++ tests/test_start_execution.py
@@ -4,41 +4,7 @@ import os
 import boto3
 from library.aws.provider import AWSProvider
 from tevico.engine.configs.config import ConfigUtils
-from tevico.engine.entities.provider.provider import Provider, CheckReport
-
-class MyProvider(Provider):
-    """Provider class for AWS."""
-
-    @property
-    def name(self) -> str:
-        """Returns the name of the provider.
-
-        Returns:
-            str: The name of the provider.
-        """
-        return "AWS"
-
-    @property
-    def metadata(self) -> Dict[str, str]:
-        """Returns metadata for the provider.
-
-        Returns:
-            Dict[str, str]: Metadata dictionary.
-        """
-        return {}
-
-    def connect(self) -> Any:
-        """Establishes a connection using AWS configuration.
-
-        Returns:
-            Any: A boto3 session object.
-        """
-        aws_config = ConfigUtils().get_config().aws_config
-
-        if aws_config is not None:
-            return boto3.Session(profile_name=aws_config['profile'])
-        
-        return boto3.Session()
+from tevico.engine.entities.provider.provider import CheckReport
 
 def get_check_reports() -> List[CheckReport]:
     """Fetches check reports by executing the provider.
@@ -46,8 +12,7 @@ def get_check_reports() -> List[CheckReport]:
     Returns:
         List[CheckReport]: A list of check reports.
     """
-    provider_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../library/aws'))
-    provider = MyProvider(path=provider_path)
+    provider = AWSProvider()
     return provider.start_execution()
 
 # Get the check reports
