037e9e13ae7c919e2222d3de05f72d5db49e7171
aafaq-rashid-comprinno <aafaq.rashid@comprinno.net>
2025-01-16 16:08:30 +0530

    added checks for cloudwatch

--- library/aws/checks/cloudwatch/cloudwatch_log_metric_filter_security_group_changes.py
+++ library/aws/checks/cloudwatch/cloudwatch_log_metric_filter_security_group_changes.py
@@ -8,11 +8,13 @@ import boto3
 import logging
 import re
 
+from requests import PreparedRequest, get
+
 from tevico.engine.entities.report.check_model import CheckReport
 from tevico.engine.entities.check.check import Check
 
 
-class cloudwatch_log_metric_filter_policy_changes(Check):
+class cloudwatch_log_metric_filter_security_group_changes(Check):
     
     def execute(self, connection: boto3.Session) -> CheckReport:
         # Initialize CloudWatch client
@@ -24,8 +26,8 @@ class cloudwatch_log_metric_filter_policy_changes(Check):
         report.passed = True
         report.resource_ids_status = {}
 
-        # Define the custom pattern for IAM policy changes (Put/Attach/Delete actions)
-        pattern = r"\$\.eventName\s*=\s*.?DeleteGroupPolicy.+\$\.eventName\s*=\s*.?DeleteRolePolicy.+\$\.eventName\s*=\s*.?DeleteUserPolicy.+\$\.eventName\s*=\s*.?PutGroupPolicy.+\$\.eventName\s*=\s*.?PutRolePolicy.+\$\.eventName\s*=\s*.?PutUserPolicy.+\$\.eventName\s*=\s*.?CreatePolicy.+\$\.eventName\s*=\s*.?DeletePolicy.+\$\.eventName\s*=\s*.?CreatePolicyVersion.+\$\.eventName\s*=\s*.?DeletePolicyVersion.+\$\.eventName\s*=\s*.?AttachRolePolicy.+\$\.eventName\s*=\s*.?DetachRolePolicy.+\$\.eventName\s*=\s*.?AttachUserPolicy.+\$\.eventName\s*=\s*.?DetachUserPolicy.+\$\.eventName\s*=\s*.?AttachGroupPolicy.+\$\.eventName\s*=\s*.?DetachGroupPolicy.?"
+        # Define the custom pattern for multiple security group change events
+        pattern = r"\$\.eventName\s*=\s*.?AuthorizeSecurityGroupIngress.+\$\.eventName\s*=\s*.?AuthorizeSecurityGroupEgress.+\$\.eventName\s*=\s*.?RevokeSecurityGroupIngress.+\$\.eventName\s*=\s*.?RevokeSecurityGroupEgress.+\$\.eventName\s*=\s*.?CreateSecurityGroup.+\$\.eventName\s*=\s*.?DeleteSecurityGroup.?"
         
         try:
             # Get all log groups in the account
@@ -44,31 +46,30 @@ class cloudwatch_log_metric_filter_policy_changes(Check):
             # Track if any log group has a matching filter
             any_matching_filter_found = False
 
-            # Check for Metric Filters for policy changes in each log group
+            # Check for Metric Filters for security group changes in each log group
             for log_group in log_groups:
                 log_group_name = log_group['logGroupName']
                 
                 # Fetch metric filters for the log group
                 filters = client.describe_metric_filters(logGroupName=log_group_name)
                 
-                # Look for filters related to IAM policy changes with the custom pattern
+                # Look for filters related to security group changes with the custom pattern
                 matching_filters = []
                 for filter in filters.get('metricFilters', []):
                     filter_pattern = filter.get('filterPattern', '')
-                    # Check if the filter pattern matches the custom pattern for policy changes
+                    # Check if the filter pattern matches the custom pattern for security group changes
                     if re.search(pattern, filter_pattern):
                         matching_filters.append(filter.get('filterName'))
 
                 if matching_filters:
                     # If a matching filter is found, update the report status and details
-                    report.resource_ids_status[f"{log_group_name} has Metric Filters for Policy Changes: [{', '.join(matching_filters)}]"] = True
+                    report.resource_ids_status[f"{log_group_name} has Metric Filters for Security Group Changes: [{', '.join(matching_filters)}]"] = True
                     any_matching_filter_found = True
-
-
+                
             # If no matching filter was found in any log group, set the report as failed
             if not any_matching_filter_found:
                 report.passed = False
-                report.resource_ids_status["No matching filters found for IAM Policy Changes in any log group"] = False
+                report.resource_ids_status["No matching filters found for Security Group Changes in any log group"] = False
 
         except Exception as e:
             logging.error(f"Error while fetching CloudWatch logs and metric filters: {e}")
