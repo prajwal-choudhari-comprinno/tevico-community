bf370f2e0b17c2953ac1b76fa89c1283c29d446b
Niraj Ranjan <64660855+niraj-ranjan-comprinno@users.noreply.github.com>
2025-01-30 23:14:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ echo -ne 'Cloning the repository...\n'
 BRANCH=${BRANCH:-main}  # Default to 'main' if BRANCH is not set
 git clone -b $BRANCH https://github.com/comprinnotech/tevico-community.git > /dev/null 2> /dev/null
 cd tevico-community
+echo -ne 'Cloning the repository1111...\n'
 git branch
 
 echo -ne 'Setting up virtual environment...\n'
