503493a890fecdbe237c5c4ac385f53f23bb6494
Comprinno Technologies <comprinnotechnologies@com015.local>
2025-02-04 17:54:11 +0530

    changes related to resource_ids_status

--- library/aws/checks/ec2/ec2_instance_managed_by_ssm.py
+++ library/aws/checks/ec2/ec2_instance_managed_by_ssm.py
@@ -40,7 +40,7 @@ class ec2_instance_managed_by_ssm(Check):
             report.status = CheckStatus.FAILED
             print("in this place")
             # BT commented report.resource_ids_status['No Instances'] = False  # No instances available
-            resource_status = (resource=AwsResource(arn="arn:aws:iam::865246394951:instance-profile/ssm", status=CheckStatus.FAILED, summary="No instances available"))
+            # resource_status = (resource=AwsResource(arn="arn:aws:iam::865246394951:instance-profile/ssm", status=CheckStatus.FAILED, summary="No instances available"))
             report.resource_ids_status = [ResourceStatus(resource=AwsResource(arn="arn:aws:iam::865246394951:instance-profile/ssm", status=CheckStatus.FAILED, summary="No instances available"))]
             return report
 
