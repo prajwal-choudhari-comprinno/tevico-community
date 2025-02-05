617655801cfbadb80607a38e0be58687d1d4edff
Comprinno Technologies <comprinnotechnologies@com015.local>
2025-02-05 18:05:07 +0530

    Updated to include framework changes related to resource_ids_status

--- library/aws/checks/ec2/ec2_ebs_snapshot_encrypted.py
+++ library/aws/checks/ec2/ec2_ebs_snapshot_encrypted.py
@@ -5,7 +5,7 @@ DATE: 10-10-2024
 
 import boto3
 
-from tevico.engine.entities.report.check_model import CheckReport, CheckStatus
+from tevico.engine.entities.report.check_model import CheckReport, CheckStatus, AwsResource, GeneralResource, ResourceStatus
 from tevico.engine.entities.check.check import Check
 
 
@@ -20,25 +20,48 @@ class ec2_ebs_snapshot_encrypted(Check):
 
         # Initialize report status
         report.status = CheckStatus.PASSED  # Assume passed unless we find an unencrypted snapshot
-        report.resource_ids_status = {}
+        report.resource_ids_status = []
 
         try:
             # Fetch all snapshots
             res = client.describe_snapshots(OwnerIds=['self'])
             snapshots = res['Snapshots']
 
+            if not snapshots:
+                report.resource_ids_status.append(
+                    ResourceStatus(
+                        resource=GeneralResource(resource=""),
+                        status=CheckStatus.SKIPPED,
+                        summary=f"No Snapshots found"
+                    )
+                )
+
             for snapshot in snapshots:
                 snapshot_id = snapshot['SnapshotId']
                 encrypted = snapshot.get('Encrypted', False)
 
                 # Log the encryption status of each snapshot
-                report.resource_ids_status[snapshot_id] = encrypted
-
+                # Snapshot only has Snapshot Id and no arn
                 if not encrypted:
-                    report.status = CheckStatus.FAILED  # If any snapshot is not encrypted, mark the report as failed
+                    report.status = CheckStatus.FAILED
+                    report.resource_ids_status.append(
+                        ResourceStatus(
+                            resource=GeneralResource(resource=snapshot_id),
+                            status=CheckStatus.FAILED,
+                            summary=f"Snapshot {snapshot_id} is not Encrypted."
+                        )
+                    )
+
 
         except Exception as e:
             report.status = CheckStatus.FAILED
-            report.resource_ids_status = {}
+            report.resource_ids_status.append(
+                ResourceStatus(
+                    resource=GeneralResource(resource=""),
+                    status=CheckStatus.FAILED,
+                    summary=f"Error in reading snapshots",
+                    exception=e
+                )
+            )
 
         return report
