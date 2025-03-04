97617dd7cf4aa885c05dc8bb262b7ff863cf3ebb
aafaq-rashid-comprinno <aafaq.rashid@comprinno.net>
2025-03-04 19:40:03 +0530

    iam_password_policy_lowercase logic fix

--- library/aws/checks/iam/iam_password_policy_lowercase.py
+++ library/aws/checks/iam/iam_password_policy_lowercase.py
@@ -1,32 +1,72 @@
 """
-AUTHOR: Supriyo Bhakat <supriyo.bhakat@comprinno.net>
-DATE: 2024-10-10
+AUTHOR: Sheikh Aafaq Rashid
+EMAIL: aafaq.rashid@comprinno.net
+DATE: 2025-01-07
 """
 
 import boto3
-
-from tevico.engine.entities.report.check_model import CheckReport, CheckStatus
+from botocore.exceptions import BotoCoreError, ClientError
+from tevico.engine.entities.report.check_model import CheckReport, CheckStatus, GeneralResource, ResourceStatus
 from tevico.engine.entities.check.check import Check
 
 
 class iam_password_policy_lowercase(Check):
 
     def execute(self, connection: boto3.Session) -> CheckReport:
-        report = CheckReport(name=__name__)
-        client = connection.client('iam')
-       
-        try:
-            password_policy = client.get_account_password_policy()
-        except client.exceptions.NoSuchEntityException:
-            report.status = CheckStatus.FAILED
-            return report
+        """
+        Check if the IAM password policy requires at least one lowercase letter.
 
-        lowercase_required = password_policy.get('RequireLowercaseCharacters', False)
-        
-      
-        if lowercase_required:
-            report.status = CheckStatus.PASSED
-        else:
-            report.status = CheckStatus.FAILED
+        :param connection: boto3 session
+        :return: CheckReport
+        """
+        # Initialize IAM client
+        client = connection.client('iam')
+
+        report = CheckReport(name=__name__)
+        report.status = CheckStatus.PASSED  # Default to passed
+        report.resource_ids_status = []
+
+        try:
+            policy = client.get_account_password_policy()['PasswordPolicy']
+            requires_lowercase = policy.get('RequireLowercaseCharacters', False)
+
+            if requires_lowercase:
+                report.resource_ids_status.append(
+                    ResourceStatus(
+                        resource=GeneralResource(name="Password Policy"),
+                        status=CheckStatus.PASSED,
+                        summary="The IAM password policy enforces the use of at least one lowercase letter."
+                    )
+                )
+            else:
+                report.status = CheckStatus.FAILED
+                report.resource_ids_status.append(
+                    ResourceStatus(
+                        resource=GeneralResource(name="Password Policy"),
+                        status=CheckStatus.FAILED,
+                        summary="The IAM password policy does not enforces the use of at least one lowercase letter."
+                    )
+                )
+
+        except client.exceptions.NoSuchEntityException:
+            report.status = CheckStatus.SKIPPED
+            report.resource_ids_status.append(
+                ResourceStatus(
+                    resource=GeneralResource(name="Password Policy"),
+                    status=CheckStatus.SKIPPED,
+                    summary="Default IAM password policy is currently configured."
+                )
+            )
+
+        except (BotoCoreError, ClientError) as e:
+            report.status = CheckStatus.ERRORED
+            report.resource_ids_status.append(
+                ResourceStatus(
+                    resource=GeneralResource(name="Password Policy"),
+                    status=CheckStatus.ERRORED,
+                    summary="An error occurred while retrieving the IAM password policy.",
+                    exception=str(e)
+                )
+            )
 
         return report
