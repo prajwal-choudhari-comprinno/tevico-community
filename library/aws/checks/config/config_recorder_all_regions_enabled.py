dd5a329679e42128e50cb6c1624331411286ac0a
aafaq-rashid-comprinno <aafaq.rashid@comprinno.net>
2025-03-21 15:12:26 +0530

    Logic Fix

--- library/aws/checks/config/config_recorder_all_regions_enabled.py
+++ library/aws/checks/config/config_recorder_all_regions_enabled.py
@@ -1,55 +1,84 @@
 """
-AUTHOR: Supriyo Bhakat
-EMAIL: supriyo.bhakat@comprinno.net
-DATE: 2024-11-15
+AUTHOR: Sheikh Aafaq Rashid
+EMAIL: aafaq.rashid@comprinno.net
+DATE: 2025-03-21
 """
 
 
 import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 
-from tevico.engine.entities.report.check_model import CheckReport, CheckStatus, GeneralResource, ResourceStatus
+from tevico.engine.entities.report.check_model import (
+    CheckReport, CheckStatus, AwsResource, GeneralResource, ResourceStatus
+)
 from tevico.engine.entities.check.check import Check
 
 
 class config_recorder_all_regions_enabled(Check):
     def execute(self, connection: boto3.Session) -> CheckReport:
         report = CheckReport(name=__name__)
-        report.status = CheckStatus.FAILED
         report.resource_ids_status = []
-        config_client = connection.client('config')
 
-        response = config_client.describe_configuration_recorder_status()
-        recorders = response.get('ConfigurationRecordersStatus', [])
+        try:
+            ec2_client = connection.client('ec2')
+            paginator = ec2_client.get_paginator('describe_regions')
+            all_regions = []
+            
+            for page in paginator.paginate():
+                all_regions.extend(region['RegionName'] for region in page['Regions'])
+            
+            enabled_regions = []
+            missing_regions = []
 
-        if not recorders:
-            report.status = CheckStatus.FAILED
-            return report
+            for region in all_regions:
+                try:
+                    regional_client = connection.client('config', region_name=region)
+                    paginator = regional_client.get_paginator('describe_configuration_recorders')
+                    recorders = []
+                    
+                    for page in paginator.paginate():
+                        recorders.extend(page.get('ConfigurationRecorders', []))
+                    
+                    if recorders:
+                        enabled_regions.append(region)
+                    else:
+                        missing_regions.append(region)
+                except (BotoCoreError, ClientError) as e:
+                    report.resource_ids_status.append(
+                        ResourceStatus(
+                            resource=GeneralResource(name=region),
+                            status=CheckStatus.UNKNOWN,
+                            summary=f"Error checking AWS Config recorder in region {region}.",
+                            exception=str(e)
+                        )
+                    )
 
-        all_regions_enabled = True
-        for recorder in recorders:
-            recorder_name = recorder['name']
-            is_enabled = recorder.get('recording', False)
-
-            if not is_enabled:
-                all_regions_enabled = False
-                report.status = CheckStatus.FAILED
+            if missing_regions:
                 report.resource_ids_status.append(
                     ResourceStatus(
-                        resource=GeneralResource(name=recorder_name),
+                        resource=GeneralResource(name="AWS Config"),
                         status=CheckStatus.FAILED,
-                        summary=''
-                    )
-                )
-            else:
-                report.resource_ids_status.append(
-                    ResourceStatus(
-                        resource=GeneralResource(name=recorder_name),
-                        status=CheckStatus.PASSED,
-                        summary=''
+                        summary=f"AWS Config recorder is not enabled in the following regions: {', '.join(missing_regions)}."
                     )
                 )
 
-        if all_regions_enabled:
-            report.status = CheckStatus.PASSED
+            if enabled_regions:
+                report.resource_ids_status.append(
+                    ResourceStatus(
+                        resource=GeneralResource(name="AWS Config"),
+                        status=CheckStatus.PASSED,
+                        summary=f"AWS Config recorder is enabled in the following regions: {', '.join(enabled_regions)}."
+                    )
+                )
+
+        except Exception as e:
+            report.resource_ids_status.append(
+                ResourceStatus(
+                    resource=GeneralResource(name="AWS Config"),
+                    status=CheckStatus.UNKNOWN,
+                    summary="Encountered an error while retrieving AWS Config recorder status.",
+                    exception=str(e)
+                )
+            )
 
         return report
